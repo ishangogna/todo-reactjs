85d61fe8070efec921d35a1873f0e00715a1d73f
Ishan Gogna <ishangogna10@gmail.com>
2020-06-28 22:25:06 +0530

    add todo feature/finishing touches

--- todo/src/App.js
+++ todo/src/App.js
@@ -10,11 +10,19 @@ class App extends Component{
   }
   deleteTodo = (id) => {
     let updatedTodoList = this.state.todos.filter(todo => {
-      return todo.id != id;
+      return todo.id !== id;
       
     })
     this.setState({
       todos: updatedTodoList,
+    })
+  }
+
+  addTodo = (todo) => {
+    todo.id = Math.random()*10;
+    let addTodoList = [...this.state.todos, todo];
+    this.setState({
+      todos: addTodoList  
     })
   }
   render(){
@@ -24,7 +32,7 @@ class App extends Component{
 
         <Todos deleteTodo = {this.deleteTodo} todos = {this.state.todos}/>
 
-        <AddTodos />
+        <AddTodos addTodo = {this.addTodo} />
       </div>
     )
   }
--- todo/src/components/addTodo.js
+++ todo/src/components/addTodo.js
@@ -2,12 +2,28 @@ import React, { Component } from 'react';
 
 class AddTodo extends Component {
     state = {
+        content : '',
+        id : '',
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            content: e.target.value,
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state);
         
     }
     render(){
         return(
             <div className = "AddTodo">
-                <h1 className = 'center blue'>Add a todo</h1>
+                <h1 className = 'center blue-text'>Add a todo</h1>
+                <form onSubmit = {this.handleSubmit}>
+                    <input onChange = {this.handleChange} id= 'task' placeholder = "Add a todo.." />
+                </form>
             </div>
         )
     }
